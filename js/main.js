8c04c23cba57741c6e98062c371825889c5b2e1c
Suzanne Zomer <suzannezomer@gmail.com>
2022-01-28 13:44:55 +0100

    Create first version of clock on homepage
    Homepage of the webpage displays a simple analog clock with the current local time of the user's webbrowser.

--- js/main.js
+++ js/main.js
@@ -51,9 +51,95 @@ async function router() {
 
   // run the productLister function (in another file)
   // if the route is '/partials/page1.html';
-  route === '/partials/page1.html' && getTimezone();
+  // route === '/partials/page1.html';
+
+  // if we are on the home page, draw the clocks
+  if (route == '/partials/home.html') {
+    console.log('loading clocks');
+
+    // measurments
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    var radius = canvas.height / 2;
+    ctx.translate(radius, radius);
+    radius = radius * 0.90;
+
+    // draw clocks every second
+    const interval = setInterval(function () {
+      drawFace(ctx, radius);
+      drawNumbers(ctx, radius);
+      drawTime(ctx, radius);
+
+    }, 1000); // every 1 second
+  }
 }
 
+// draw clock components
+function drawFace(ctx, radius) {
+  var grad;
+  ctx.beginPath();
+  ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+  grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
+  grad.addColorStop(0, '#333');
+  grad.addColorStop(0.5, 'white');
+  grad.addColorStop(1, '#333');
+  ctx.strokeStyle = grad;
+  ctx.lineWidth = radius * 0.1;
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
+  ctx.fillStyle = '#333';
+  ctx.fill();
+}
+function drawNumbers(ctx, radius) {
+  var ang;
+  var num;
+  ctx.font = radius * 0.15 + "px arial";
+  ctx.textBaseline = "middle";
+  ctx.textAlign = "center";
+  for (num = 1; num < 13; num++) {
+    ang = num * Math.PI / 6;
+    ctx.rotate(ang);
+    ctx.translate(0, -radius * 0.85);
+    ctx.rotate(-ang);
+    ctx.fillText(num.toString(), 0, 0);
+    ctx.rotate(ang);
+    ctx.translate(0, radius * 0.85);
+    ctx.rotate(-ang);
+  }
+}
+function drawTime(ctx, radius) {
+  var now = new Date();
+  var hour = now.getHours();
+  var minute = now.getMinutes();
+  var second = now.getSeconds();
+  //hour
+  hour = hour % 12;
+  hour = (hour * Math.PI / 6) +
+    (minute * Math.PI / (6 * 60)) +
+    (second * Math.PI / (360 * 60));
+  drawHand(ctx, hour, radius * 0.5, radius * 0.07);
+  //minute
+  minute = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60));
+  drawHand(ctx, minute, radius * 0.8, radius * 0.07);
+  // second
+  second = (second * Math.PI / 30);
+  drawHand(ctx, second, radius * 0.9, radius * 0.02);
+}
+function drawHand(ctx, pos, length, width) {
+  ctx.beginPath();
+  ctx.lineWidth = width;
+  ctx.lineCap = "round";
+  ctx.moveTo(0, 0);
+  ctx.rotate(pos);
+  ctx.lineTo(0, -length);
+  ctx.stroke();
+  ctx.rotate(-pos);
+}
+
+// display menu choice
 function makeMenuChoiceActive(route) {
   // change active link in the menu
   let aTagsInNav = document.querySelectorAll('nav a');
@@ -66,6 +152,8 @@ function makeMenuChoiceActive(route) {
     }
   }
 }
+
+
 
 // Router function will be called when popstate function called
 // Called when back or forward buttons are used 
